808aeea531621ba7f29d19f206c6df179ee6e0cd
Bhanu Kiran <bhanukiran809@gmail.com>
2023-03-03 13:31:05 +0530

    added 5 names

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>this is rep3 project</h1>
+      <h1>name1</h1>
+      <h1>name2</h1>
+      <h1>name3</h1>
+      <h1>name4</h1>
+      <h1>name5</h1>
     </div>
   );
 }
